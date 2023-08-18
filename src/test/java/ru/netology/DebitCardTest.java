f9bf1f91f5d77a2ce152ba2116ecc23accc64d3f
Jenek <Jenek619@gmail.com>
2023-08-18 12:21:24 +0300

    Second commit

---
+++ src/test/java/ru/netology/DebitCardTest.java
@@ -0,0 +1,15 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class DebitCardTest {
+
+    @Test
+    void shouldTestDebitCard() {
+        open("http//localhost:9999");
+
+    }
+
+}
