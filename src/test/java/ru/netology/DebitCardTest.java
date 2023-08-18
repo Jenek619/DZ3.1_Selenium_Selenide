52778ab244f2248c0087ad1872bc225e7dca1e33
Jenek <Jenek619@gmail.com>
2023-08-18 15:00:33 +0300

    CardTest

--- src/test/java/ru/netology/DebitCardTest.java
+++ src/test/java/ru/netology/DebitCardTest.java
@@ -1,11 +1,10 @@
 package ru.netology;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -24,7 +23,7 @@ public class DebitCardTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
@@ -35,7 +34,13 @@ public class DebitCardTest {
 
     @Test
     void shouldDebitCardTest() {
-        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Чистов Евгений");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+7980000000");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[button]")).click();
+        String text = driver.findElement(By.className("order-success")).getText();
+        Assertions.assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+
 
     }
 
