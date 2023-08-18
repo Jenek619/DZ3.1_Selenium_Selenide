a1698c4a9bea0d2c3f7947bcf1941a8dffa1260f
Jenek <Jenek619@gmail.com>
2023-08-18 14:11:33 +0300

    Commit 3

---
+++ README.md
@@ -0,0 +1,4 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/sa2wd2exfb4evjw4?svg=true)](https://ci.appveyor.com/project/Jenek619/dz3-1-selenium-selenide)
+
+
+
--- src/test/java/ru/netology/DebitCardTest.java
+++ src/test/java/ru/netology/DebitCardTest.java
@@ -1,15 +1,43 @@
 package ru.netology;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Selenide.open;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DebitCardTest {
+    private WebDriver driver;
 
-    @Test
-    void shouldTestDebitCard() {
-        open("http//localhost:9999");
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
 
     }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldDebitCardTest() {
+        driver.get("http://localhost:9999");
+
+    }
+
 
 }
