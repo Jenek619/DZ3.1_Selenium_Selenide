921ca819e20abcdcd305583435d590ec575daaf5
Jenek <Jenek619@gmail.com>
2023-08-19 13:41:49 +0300

    Commit

--- src/test/java/ru/netology/DebitCardTest.java
+++ src/test/java/ru/netology/DebitCardTest.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DebitCardTest {
-    private WebDriver driver;
+    public WebDriver driver;
 
     @BeforeAll
     public static void setupAll() {
@@ -16,7 +16,7 @@ public class DebitCardTest {
     }
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
@@ -25,20 +25,21 @@ public class DebitCardTest {
     }
 
     @AfterEach
-    void tearDown() {
+    public void tearDown() {
         driver.quit();
         driver = null;
     }
 
+
     @Test
-    void shouldDebitCardTest() {
+    public void shouldDebitCardTest() {
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Чистов Евгений");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+7980000000");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.cssSelector("button")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
         var actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
-        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",actualText);
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
 
 
     }
